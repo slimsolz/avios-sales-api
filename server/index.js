1f39f28e57f679238eccbec19cabbdf3d2e620c9
solomon <odumahs@gmail.com>
2021-01-16 15:20:23 +0100

    add test

--- server/index.js
+++ server/index.js
@@ -16,14 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/v1", index);
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  // if (err.parent.errno === "ECONNREFUSED") {
-  // return res.status(500).json({
-  // success: false,
-  // message: err.message,
-  // });
-  // }
-
   return res.status(400).json({
     success: false,
     message: err,
